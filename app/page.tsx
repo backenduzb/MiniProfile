b531c9c4cbb7f10986cda18c26f77e96f5703473
Qobiljon <qobiljonsharipov33@gmail.com>
2025-10-31 13:09:55 +0500

    Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,49 @@
 "use client";
 
-import React from "react";
 import Image from "next/image";
 import ProfilePhoto from "@/public/photo.jpg";
 import Link from "next/link";
-import Uzbekdevs from "@/public/uzdevs.png"
 import X from "@/public/x.png"
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+  if (loading) {
+    return (
+      <>
+        <div className="fixed inset-0 flex">
+          <motion.div
+            initial={{ x: 0 }}
+            animate={{ x: "-100%" }}
+            transition={{ duration: 1, ease: "easeInOut", delay: 1 }}
+            className="w-1/2 h-full bg-black/90 border-r"
+          />
+
+          <div className="fixed inset-0 flex items-center justify-center z-[100]">
+            <div className="relative h-18 w-18">
+              <div className="absolute inset-0 border z-100"></div>
+              <div className="absolute inset-0 border animate-spin z-100"></div>
+            </div>
+          </div>
+
+
+          <motion.div
+            initial={{ x: 0 }}
+            animate={{ x: "100%" }}
+            transition={{ duration: 1, ease: "easeInOut", delay: 1 }}
+            className="w-1/2 h-full bg-black/90 border-l"
+          />
+        </div>
+
+      </>
+    );
+  }
   return (
     <>
       <div className="relative w-full h-screen overflow-hidden">
